471f51f64488ac30de80c15e975e4165302067b6
Pentagon <pentagonalpentagon@gmail.com>
2018-07-04 07:50:54 -0400

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
-# Astral-Chat
-here's chatty
+# Astral Chat
+
+![Art by Kacchan#8317](https://i.imgur.com/04ljwEO.png)
+
+<sup>Art by Kacchan#8317</sup>
+***
+
+[Please funnel money into my mouth to support development  by clicking here](https://ko-fi.com/codeagon)
+***
+
+## What this?
+Astral Chat (AC) is the dumbed down, somehow even more poorly written, cousin of [Astral TERA, the server within a server](https://github.com/codeagon/Astral-TERA/blob), which you should totes go check out thanks. The goal of AC is to provide players (mainly in guilds), the ability to talk to people on discord without the risk of being banned that comes with [TERA Discord Relay](https://github.com/meishuu/tera-discord-relay).
+
+## How work?
+Astral Chat works by sending messages sent on the `/6`chat channel to a third party server hosted by (hopefully) someone you trust. The messages are then sent to a discord bot (again hosted by someone). Essentially allowing people in-game the ability to talk to people on discord and visa versa. This also works without the discord component and across servers, allowing for a cross-server and cross region chat to exist also. Also you can watch cat videos together.
+
+## How use?
+### Client
+After first running the mod to generate your ID, change the `serverHost` IP  and port from `localhost` to the IP of the person hosting the server component in `config.json`. When joining the game, you should connect automatically to the chat. To start chatting, simply type any messages into `/6`.
+
+Should you get disconnected from the chat, simply typing `!ac connect` into the chat or `ac connect` into `/8` should reconnect you.
+
+### Server
+To setup the server, simply download the pre-packed release because I am too lazy to write a package.json. You will need to run the server first, followed by the discord bot. You will need to edit the `config.json` for the discord bot, instructions for setting one up can be found [here](https://github.com/Chikachi/DiscordIntegration/wiki/How-to-get-a-token-and-channel-ID-for-Discord). You'll need to give the config your token as well as the channel too.
+
+## I hate??
+Me too. Feel free to fork it, I am highly aware this is garbo, I stripped a lot and rewrote some parts worse for some reason when cutting down AT for this. If you have suggestions feel free to leave a message in [this discord chat](https://discord.gg/dzB7xZK). I will most likely not be updating this super often, as AT is the main project.
+
+
---
+++ index.js
@@ -0,0 +1,192 @@
+/* global __dirname */
+
+const Command = require('command');
+const path = require('path');
+const fs = require('fs');
+const GameState = require('tera-game-state');
+const Networking = require('./networking');
+const networked = new Map();
+const PRIVATE_CHANNEL_ID = -3 >>> 0; // wow a number!
+const PRIVATE_CHANNEL_NAME = `Chat`;
+module.exports = function HiWelcomeToMyMinecraftLetsPlayImYourHostHUUUUUUUUUUGEEEEDOOOONNNG(dispatch) {
+    const command = Command(dispatch);
+    const game = GameState(dispatch);
+    const net = new Networking();
+    let website = [{link: 4012, linkKey: 'google.com'}], // So that websites have a backup or something
+            online = false;
+    try {
+        config = require('./config.json');
+    } catch (e) {
+        config = {
+            "discordMessages": true,
+            "allowWebLinks": true,
+            "myName": "Anonymous",
+            "Id": rand(1, 999999999999999), //nine
+            "serverHost": "localhost", //yes change this thanks
+            "serverPort": 3454
+        };
+        saveConfig();
+    }
+
+//funcs
+
+    function rand(min, max) {
+        return Math.floor(Math.random() * (max - min + 1) + min);
+    }
+
+    function strip(str) { //fantastic regex lmozle
+        return str.replace(/^<[^>]+>|<\/[^>]+><[^\/][^>]*>|<\/[^>]+>$/g, '');
+    }
+
+    function saveConfig() {
+        fs.writeFile(path.join(__dirname, 'config.json'), JSON.stringify(
+                config, null, 4), err => {
+            console.log('[[ASTRAL CHAT]] - CONFIG FILE CREATED');
+        });
+    }
+
+    function message(msg) {
+        command.message(`<font color="#ffc5e8">  [Astral Chat] - </font> <font color="#c5ffdc ">${msg}`);
+    }
+
+    function chat(userName, msg) {
+        dispatch.toClient('S_PRIVATE_CHAT', 1, {
+            channel: PRIVATE_CHANNEL_ID,
+            authorID: 0,
+            authorName: userName,
+            message: msg
+        });
+    }
+
+
+    function joinChat() {
+        dispatch.toClient('S_JOIN_PRIVATE_CHANNEL', 2, {
+            index: 5,
+            channelId: PRIVATE_CHANNEL_ID,
+            unk: [],
+            name: PRIVATE_CHANNEL_NAME
+        });
+    }
+//commands
+    command.add('ac', (cmd, arg, arg2, arg3, arg4, arg5, arg6, arg7) => {
+        switch (cmd) {
+            case 'connect':
+            case 'reconnect':
+                online = true;
+                net.connect({
+                    host: config.serverHost,
+                    port: config.serverPort
+                });
+                net.send('login', config.id);
+                message(`Connected`);
+                break
+            case 'disconnect':
+                net.send('logout');
+                setTimeout(function () {
+                    net.close();
+                    message('Disconnected');
+                }, 1000);
+                break
+            case 'username':
+            case 'user':
+            case 'name':
+                config.myName = arg;
+                message(`Username set to "${config.myName}"`);
+                saveConfig();
+                break
+            default:
+                message('WOOOpssies doopsies you typed a command wrong try again !!!!!!!!!!!!');
+        }
+    });
+    //hooks
+
+    dispatch.hook('S_LOAD_CLIENT_USER_SETTING', 1, () => {
+        message('uwuuwuwuuuuu');
+        process.nextTick(() => {
+            joinChat();
+        });
+    });
+
+    dispatch.hook('S_LOGIN', 10, () => {
+        net.send('login', config.id);
+        online = true;
+    });
+    
+    dispatch.hook('S_JOIN_PRIVATE_CHANNEL', 2, event => event.index === 5 ? false : undefined);
+    dispatch.hook('C_LEAVE_PRIVATE_CHANNEL', 1, event => event.index === 5 ? false : undefined);
+    dispatch.hook('C_REQUEST_PRIVATE_CHANNEL_INFO', 2, event => {
+        if (event.channelId === PRIVATE_CHANNEL_ID) {
+            dispatch.toClient('S_REQUEST_PRIVATE_CHANNEL_INFO', 2, {
+                owner: 1,
+                password: 0,
+                members: [],
+                friends: []
+            });
+            return false;
+        }
+    });
+
+    dispatch.hook('C_CHAT', 1, {order: -9}, event => {
+        if (event.channel === 16) {
+            net.send('chat', config.myName, strip(event.message), 0);
+            return false;
+        }
+    });
+
+    dispatch.hook('C_SHOW_ITEM_TOOLTIP_EX', 2, (event) => {
+        tooltipId = event.id.toString();
+        for (let i of website) {
+            if (!config.allowWebLinks)
+                return;
+            if (tooltipId.includes(i.linkKey)) {
+                dispatch.toClient('S_SHOW_AWESOMIUMWEB_SHOP', 1, {
+                    link: i.link.toString()
+                });
+                return false;
+            }
+        }
+    });
+
+    dispatch.hook('S_RETURN_TO_LOBBY', 1, () => {
+        online = false;
+        net.send('logout');
+        dispatch.hookOnce('C_LOAD_TOPO_FIN', 1, () => { //wew
+            net.send('login', config.Id);
+        });
+    });
+    //nets
+
+    net.on('chat', (userName, msg, discord) => {
+        if (online) {
+            if (!config.discordMessages && discord === 2)
+                return;
+            chat(userName, msg);
+        }
+    });
+    net.on('weburl', (link, linkKey) => {
+        website.push(website, {link, linkKey});
+    });
+    net.on('add', (id) => {
+        addUser(id);
+    });
+    net.on('error', (err) => {
+        console.warn(err);
+    });
+    net.on('ping', () => {
+        net.send('pong');
+    });
+    net.connect({
+        host: config.serverHost,
+        port: config.serverPort
+    });
+    function addUser(id, user = {}) {
+        networked.set(id, user);
+    }
+
+
+    //trashes
+    this.destructor = () => {
+        net.close();
+        command.remove('ac'); // since this doesn't need anything we can do reloading stuff
+    };
+};
